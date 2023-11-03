5c591673cc4c359a25b50838d1df01e152f9367f
trecscalane <achima_accg@hotmail.com>
2023-11-03 22:33:02 +0100

    style.css

--- portofolio.js
+++ portofolio.js
@@ -42,11 +42,3 @@ function closeMenu() {
 
 
 
-/** dakr mode toogle */
-
-$(document).ready(function() {
-    $('#toggle-theme').click(function() {
-        $('body').toggleClass('dark-mode');
-        $('header, main, footer, nav a').toggleClass('dark-mode');
-    });
-});
